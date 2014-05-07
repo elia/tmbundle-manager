c8b9e8f457b90295ce7d51df42245208c77b56fe
Elia Schito <elia@schito.me>
2014-05-08 00:07:39 +0200

    Let's say I'm adding specs

---
+++ spec/tmbundle_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe TMBundle::BundleName do
+  subject(:bundle_name) {  }
+
+  context 'with a dashed name' do
+    let(:name) { 'textmate/ruby-on-rails-tmbundle' }
+  end
+end
